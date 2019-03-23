ec49885a34bfb7dda48205d0770f549a045002e3
Carlos Arias-Miranda <carlosar1219@hotmail.com>
2019-03-23 19:54:56 -0400

    Style signup form and footer

--- frontend/components/session/logged_out.jsx
+++ frontend/components/session/logged_out.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './header/header';
 import Signup from './signup/signup';
+import { Link } from 'react-router-dom';
 
 export default (props) => {
     return (
         <main>
             <Header />
-            <section className="flex-center">
-                <section className="container flex-space-between">
+            <section className="flex-center outer-login-body">
+                <section className="container flex-space-between login-body">
                     <article className="about">
                         <h2>Connect with friends and the world around you on Fakebook.</h2>
 
@@ -29,6 +30,61 @@ export default (props) => {
                     <Signup />
                 </section>
             </section>
+            <footer className="flex-center main-footer">
+                <section className="container">
+                    <section className="top-footer">
+                        <span>English (US)</span>
+                        <Link to="/"> Español</Link>
+                        <Link to="/"> Français (France)</Link>
+                        <Link to="/"> 中文(简体)</Link>
+                        <Link to="/"> العربية</Link>
+                        <Link to="/"> Português (Brasil)</Link>
+                        <Link to="/"> Italiano</Link>
+                        <Link to="/"> 한국어</Link>
+                        <Link to="/"> Deutsch</Link>
+                        <Link to="/"> हिन्दी</Link>
+                        <Link to="/"> 日本語</Link>
+                    </section>
+                    <hr />
+                    <section className="sub-footer">
+                        <Link to="/"> Sign Up</Link>
+                        <Link to="/"> Log In</Link>
+                        <Link to="/"> Messenger</Link>
+                        <Link to="/"> Facebook Lite</Link>
+                        <Link to="/"> Find</Link>
+                        <Link to="/"> Friends</Link>
+                        <Link to="/"> People</Link>
+                        <Link to="/"> Profiles</Link>
+                        <Link to="/"> Pages</Link>
+                        <Link to="/"> Page Categories</Link>
+                        <Link to="/"> Places</Link>
+                        <Link to="/"> Games</Link>
+                        <Link to="/"> Locations</Link>
+                        <Link to="/"> Marketplace</Link>
+                    </section>
+                    <section className="sub-footer">
+                        <Link to="/"> Groups</Link>
+                        <Link to="/"> Instagram</Link>
+                        <Link to="/"> Local</Link>
+                        <Link to="/"> Fundraisers</Link>
+                        <Link to="/"> About</Link>
+                        <Link to="/"> Create Page</Link>
+                        <Link to="/"> Create Ad</Link>
+                        <Link to="/"> Developers</Link>
+                        <Link to="/"> Careers</Link>
+                        <Link to="/"> Privacy</Link>
+                        <Link to="/"> Cookies</Link>
+                        <Link to="/"> Ad Choices</Link>
+                        <Link to="/"> Terms</Link>
+                    </section>
+                    <section className="sub-footer">
+                        <Link to="/"> Account Security</Link>
+                        <Link to="/"> Login Help</Link>
+                        <Link to="/"> Help</Link>
+                    </section>
+                    <span className="copyright">Fakebook © 2019</span>
+                </section>
+            </footer>
         </main>
     );
 };
--- frontend/components/session/signup/signup.jsx
+++ frontend/components/session/signup/signup.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default (props) => {
     return (
-        <div className="signup">
-            
-        </div>
+        <section className="signup">
+            <h1>Sign Up</h1>
+            <h3>It's free and always will be.</h3>
+
+            <form className="signup-form">
+                <input type="text" className="name-input" placeholder="First name" />
+                <input type="text" className="name-input" placeholder="Last name" />
+                <input type="text" placeholder="Mobile number or email" />
+                <input type="password" placeholder="New password" />
+
+                <label className="birthday">Birthday<br />
+                    <select>
+                        <option value="Month">Month</option>
+                    </select>
+                    <select>
+                        <option value="23">23</option>
+                    </select>
+                    <select>
+                        <option value="1994">1994</option>
+                    </select>
+
+                    <span className="why-birthday">
+                        Why do I need to provide my birthday?
+                    </span>
+                </label>
+
+                <div className="gender">
+                    <input type="radio"/>Female
+                    <input type="radio" />Male
+                </div>
+
+                <p className="fineprint">
+                    By clicking Sign Up, you agree to our 
+                    <Link to="/"> Terms</Link>, <Link to="/"> Data Policy</Link> and 
+                    <Link to="/"> Cookies Policy.</Link> You may receive 
+                    SMS Notifications from us and can opt out any time.
+                </p>
+
+                <input type="submit" value="Sign Up" />
+
+                <footer class="signup-footer">
+                    <hr />
+                    <p>
+                        <Link to="/">Create a Page </Link>
+                        for a celebrity, band or business.
+                    </p>
+                </footer>
+            </form>
+        </section>
     );
 };
