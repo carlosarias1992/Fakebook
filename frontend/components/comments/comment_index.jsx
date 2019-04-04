135fc13f9a20c16b96fe713dbe5b813458b3b551
Carlos <carlosar1219@hotmail.com>
2019-04-04 11:15:10 -0400

    Fix comments bug

--- frontend/components/comments/comment_index.jsx
+++ frontend/components/comments/comment_index.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import CommentIndexItemContainer from './comment_index_item_container';
 
 export default props => {
-  const comments = props.post.comments_id.map(commentId => {
-    return <CommentIndexItemContainer commentId={commentId} key={commentId} />;
-  });
+  let comments;
+
+  if (props.post.comments_id) {
+    comments = props.post.comments_id.map(commentId => {
+      return <CommentIndexItemContainer commentId={commentId} key={commentId} />;
+    });
+  } else {
+    comments = [];
+  }
 
   return (
     <div>{comments}</div>
