89d8b84a40d8e849a37a7cb8280c0a4db7e1c44a
Carlos <carlosar1219@hotmail.com>
2019-04-05 09:15:25 -0400

    Fix heroku likes bug

--- frontend/components/likes/likes.jsx
+++ frontend/components/likes/likes.jsx
@@ -7,8 +7,8 @@ export default props => {
         let likersToString;
 
         if (likes.length > 2) {
-            const liker1 = likers[likes[0].user_id];
-            const liker2 = likers[likes[1].user_id];
+            const liker1 = likers[likes[0].user_id] || {};
+            const liker2 = likers[likes[1].user_id] || {};
             const remaining = likes.length - 2;
 
             if (remaining === 1) {
@@ -17,12 +17,12 @@ export default props => {
                 likersToString = `${liker1.first_name} ${liker1.last_name}, ${liker2.first_name} ${liker2.last_name} and ${remaining} others`;
             }
         } else if (likes.length === 2) {
-            const liker1 = likers[likes[0].user_id];
-            const liker2 = likers[likes[1].user_id];
+            const liker1 = likers[likes[0].user_id] || {};
+            const liker2 = likers[likes[1].user_id] || {};
 
             likersToString = `${liker1.first_name} ${liker1.last_name} and ${liker2.first_name} ${liker2.last_name}`;
         } else if (likes.length === 1) {
-            const liker = likers[likes[0].user_id];
+            const liker = likers[likes[0].user_id] || {};
             likersToString = `${liker.first_name} ${liker.last_name}`;
         } else {
             likersToString = "";
