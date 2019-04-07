c85b1dedf082ef3a17c871971386a2aa667077eb
Carlos <carlosar1219@hotmail.com>
2019-04-07 11:38:49 -0400

    Fix heroku loading issue

--- frontend/components/photos/photos_container.js
+++ frontend/components/photos/photos_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Photos from './photos';
 
 const mapStateToProps = (state, ownProps) => {
-    const user = ownProps.user;
+    const user = ownProps.user || {};
     const photos = state.entities.users[user.id].photos;
 
     return {
