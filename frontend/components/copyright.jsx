56cd50b9ed805e45760aca6230849cd077777b8b
Carlos <carlosar1219@hotmail.com>
2019-04-09 12:11:14 -0400

    Update footer disclosure

--- frontend/components/copyright.jsx
+++ frontend/components/copyright.jsx
@@ -7,9 +7,9 @@ export default props => {
                 English (US)
             </div>
             <footer>
-                Fakebook 2019. Connect with me 
+                Fakebook 2019 by Carlos Arias-Miranda. Connect with me 
                 on <a target="_blank" href="https://www.linkedin.com/in/carlos-arias-miranda-a98025172/">LinkedIn</a> or
-                <a target="_blank" href="https://github.com/carlosarias1992"> Github</a>.
+                <a target="_blank" href="https://github.com/carlosarias1992"> Github</a>. Inspired by Facebook.
             </footer>
         </div>
     )
