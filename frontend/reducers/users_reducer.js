639f7d6d5358d0077918c110798fc59d2dfd47c1
Carlos <carlosar1219@hotmail.com>
2019-04-09 20:54:28 -0400

    Fix receive request issue

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,6 +1,7 @@
 import { merge } from 'lodash';
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { REMOVE_POST, RECEIVE_POST } from '../actions/posts_actions';
+import { RECEIVE_FRIEND_REQUEST } from '../actions/friend_request_actions';
 import {
     RECEIVE_USERS,
     RECEIVE_USER
@@ -21,6 +22,22 @@ export default (state = {}, action) => {
         case RECEIVE_USER:
             newState = { [user.id]: user };
             return merge({}, oldState, newState);
+        case RECEIVE_FRIEND_REQUEST:
+            const { request } = action;
+            const requestValue = Object.values(request)[0];
+            const senderFriends = merge({}, oldState[requestValue.sender_id]).friends_id;
+            const receiverFriends = merge({}, oldState[requestValue.receiver_id]).friends_id;
+            let sender, receiver;
+            
+            if(requestValue.status === "accepted") {
+                senderFriends.push(requestValue.receiver_id);
+                sender = { [requestValue.sender_id]: { friends_id: senderFriends } };
+
+                receiverFriends.push(requestValue.sender_id);
+                receiver = { [requestValue.receiver_id]: { friends_id: receiverFriends } };
+            } 
+
+            return merge({}, oldState, sender, receiver);
         case RECEIVE_POST: 
             updatedUser = merge({}, oldState[action.post.author_id]);
             
